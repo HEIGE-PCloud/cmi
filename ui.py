411037c1080a8a0dee3f774faa5400277edab9fa
Junrong Chen <junrong.chen@imc.com>
2024-07-24 22:34:01 +0200

    fix: make ip address a variable

--- ui.py
+++ ui.py
@@ -2,6 +2,7 @@ from bokeh.server.server import Server
 from tornado.ioloop import IOLoop
 from bokeh.document import Document
 
+IP_ADDRESS = "127.0.0.1"
 
 def ui_root(doc: Document):
     pass
@@ -12,11 +13,11 @@ def start_ui():
         {"/": ui_root},
         num_procs=1,
         io_loop=IOLoop(),
-        address="127.0.0.1",
+        address=IP_ADDRESS,
         allow_websocket_origin=["*"],
     )
     server.start()
-    print("Starting turbot ui on http://0.0.0.0:5006/")
+    print(f"Starting turbot ui on http://{IP_ADDRESS}:5006/")
     server.io_loop.add_callback(server.show, "/")
     server.io_loop.start()
 
