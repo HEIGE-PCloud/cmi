9149d5e6e6dc5ab5a315b4841afd708877353ee7
PCloud <heige.pcloud@outlook.com>
2024-07-27 01:41:38 +0200

    feat: implement option pricer

--- cards.py
+++ cards.py
@@ -57,8 +57,9 @@ class Cards:
         13.0,
     ]
 
-    def __init__(self):
+    def __init__(self, total_cards_to_choose=20):
         self._chosen_cards = []
+        self._total_cards_to_choose = total_cards_to_choose
 
     def choose_card(self, card: float):
         self._chosen_cards.append(card)
@@ -66,11 +67,27 @@ class Cards:
     def set_chosen_cards(self, cards: List[float]):
         self._chosen_cards = cards
 
+    def get_chosen_cards_sum(self) -> float:
+        return sum(self._chosen_cards)
+
     def get_remaining_cards(self) -> List[float]:
         remaining_cards = self.all_cards.copy()
         for x in self._chosen_cards:
             remaining_cards.remove(x)
         return remaining_cards
+
+    def get_remaining_cards_to_choose(self) -> int:
+        return self._total_cards_to_choose - len(self._chosen_cards)
+
+    def get_expected_value(self) -> float:
+        cards = self.get_remaining_cards()
+        return sum(cards) / len(cards)
+
+    def get_theoretical_price(self) -> float:
+        return (
+            self.get_expected_value() * self.get_remaining_cards_to_choose()
+            + self.get_chosen_cards_sum()
+        )
 
 
 def test_choose_cards():
@@ -130,8 +147,20 @@ def test_choose_cards():
         13.0,
         13.0,
     ]
+    assert cards.get_remaining_cards_to_choose() == 17
+
 
 def test_choose_card():
-  cards = Cards()
-  cards.set_chosen_cards(cards.all_cards)
-  assert cards.get_remaining_cards() == []
+    cards = Cards()
+    cards.set_chosen_cards(cards.all_cards)
+    assert cards.get_remaining_cards() == []
+
+
+def test_theoretical_price():
+    cards = Cards()
+    cards.set_chosen_cards([1.0, 2.0, 3.0])
+    assert cards.get_theoretical_price() == 1.0 + 2.0 + 3.0 + cards.get_expected_value() * 17
+
+    cards2 = Cards()
+    assert cards2.get_expected_value() == 7.0
+    assert cards2.get_theoretical_price() == 140
--- option_pricing.py
+++ option_pricing.py
@@ -1,89 +1,138 @@
 import numpy as np
 import time
 from black_scholes import call_value, call_delta, put_value, put_delta
-
-all_cards = [
-    1.0, 1.0, 1.0, 1.0,
-    2.0, 2.0, 2.0, 2.0,
-    3.0, 3.0, 3.0, 3.0,
-    4.0, 4.0, 4.0, 4.0,
-    5.0, 5.0, 5.0, 5.0,
-    6.0, 6.0, 6.0, 6.0,
-    7.0, 7.0, 7.0, 7.0,
-    8.0, 8.0, 8.0, 8.0,
-    9.0, 9.0, 9.0, 9.0,
-    10.0, 10.0, 10.0, 10.0,
-    11.0, 11.0, 11.0, 11.0,
-    12.0, 12.0, 12.0, 12.0,
-    13.0, 13.0, 13.0, 13.0
-]
-
-chosen_cards = []
-
-total_cards_to_choose = 20
-remaining_cards_to_choose = total_cards_to_choose - len(chosen_cards)
-remaining_cards = all_cards.copy()
-for x in chosen_cards:
-    remaining_cards.remove(x)
-
-cards = np.array(remaining_cards)
-ev = np.mean(cards)
-chosen_cards_sum = sum(chosen_cards)
-theo_price = chosen_cards_sum + ev * remaining_cards_to_choose
+from cards import Cards
+import statistics
 
 
 def call_payoff(underlying, strike):
     return np.maximum(underlying - strike, 0)
 
+
 def put_payoff(underlying, strike):
     return np.maximum(strike - underlying, 0)
 
 
-start_time = time.time()
+def option_pricing(call_strike: float, put_strike: float, cards: Cards):
+    remaining_cards = np.array(cards.get_remaining_cards())
+    cnt = 100000
 
-cnt = 100000
+    # Generate a matrix of shuffled cards
+    shuffled_cards = np.array(
+        [np.random.permutation(remaining_cards) for _ in range(cnt)]
+    )
 
-# Generate a matrix of shuffled cards
-shuffled_cards = np.array([np.random.permutation(cards) for _ in range(cnt)])
+    # Sum the first 20 elements of each shuffle
+    sums = (
+        shuffled_cards[:, : cards.get_remaining_cards_to_choose()].sum(axis=1)
+        + cards.get_chosen_cards_sum()
+    )
 
-# Sum the first 20 elements of each shuffle
-sums = shuffled_cards[:, :remaining_cards_to_choose].sum(axis=1) + chosen_cards_sum
+    # Calculate call and put values
+    call_totals = call_payoff(sums, call_strike)
+    put_totals = put_payoff(sums, put_strike)
 
-# Calculate call and put values
-call_totals = call_payoff(sums, 150)
-put_totals = put_payoff(sums, 130)
+    # Average the totals
+    call_price = call_totals.mean()
+    put_price = put_totals.mean()
+    return call_price, put_price
 
-# Average the totals
-call_price = call_totals.mean()
-put_price = put_totals.mean()
 
-vol_low = 0.0001
-vol_high = 1.0
-for _ in range(30):
-    vol_mid = (vol_low + vol_high) / 2
-    res = call_value(theo_price, 150, remaining_cards_to_choose, 0, vol_mid)
-    if res > call_price:
-        vol_high = vol_mid
-    else:
-        vol_low = vol_mid
+def get_call_vol(cards: Cards, strike: float, iterations: int = 30):
+    vol_low = 0.0001
+    vol_high = 1.0
+    underlying_price = cards.get_theoretical_price()
+    remaining_cards_to_choose = cards.get_remaining_cards_to_choose()
+    for _ in range(iterations):
+        vol_mid = (vol_low + vol_high) / 2.0
+        res = call_value(
+            underlying_price,
+            strike,
+            remaining_cards_to_choose,
+            0.0,
+            vol_mid,
+        )
+        if res > call_price:
+            vol_high = vol_mid
+        else:
+            vol_low = vol_mid
+    return vol_low
 
-print("Theo: ", theo_price)
-print("150 Call: ", call_price)
-print("130 Put:", put_price)
-print("Volatility:", vol_low)
-print("Estimated 150 Call:", call_value(theo_price, 150, remaining_cards_to_choose, 0, vol_low))
-print("Call Delta:", call_delta(theo_price, 150, remaining_cards_to_choose, 0, vol_low))
-vol_low = 0.0001
-vol_high = 1.0
-for _ in range(30):
-    vol_mid = (vol_low + vol_high) / 2
-    res = put_value(theo_price, 130, remaining_cards_to_choose, 0, vol_mid)
-    if res > call_price:
-        vol_high = vol_mid
-    else:
-        vol_low = vol_mid
-print("Volatility:", vol_low)
-print("Estimated 130 Put:", put_value(theo_price, 130, remaining_cards_to_choose, 0, vol_low))
-print("Put Delta:", put_delta(theo_price, 130, remaining_cards_to_choose, 0, vol_low))
-end_time = time.time()
-print("Time taken:", end_time - start_time, "seconds")
+
+def get_put_vol(cards: Cards, strike: float, iterations: int = 30):
+    vol_low = 0.0001
+    vol_high = 1.0
+    underlying_price = cards.get_theoretical_price()
+    remaining_cards_to_choose = cards.get_remaining_cards_to_choose()
+    for _ in range(iterations):
+        vol_mid = (vol_low + vol_high) / 2.0
+        res = put_value(
+            underlying_price,
+            strike,
+            remaining_cards_to_choose,
+            0.0,
+            vol_mid,
+        )
+        if res > call_price:
+            vol_high = vol_mid
+        else:
+            vol_low = vol_mid
+    return vol_low
+
+if __name__ == "__main__":
+    start_time = time.time()
+
+    cards = Cards()
+    call_price, put_price = option_pricing(150, 130, cards)
+    call_vol = get_call_vol(cards, 150)
+    put_vol = get_put_vol(cards, 130)
+    print("Theo: ", cards.get_theoretical_price())
+    print("150 Call: ", call_price)
+    print(
+        "Estimated 150 Call:",
+        call_value(
+            cards.get_theoretical_price(),
+            150,
+            cards.get_remaining_cards_to_choose(),
+            0,
+            call_vol,
+        ),
+    )
+
+    print("130 Put:", put_price)
+    print(
+        "Estimated 130 Put:",
+        put_value(
+            cards.get_theoretical_price(),
+            130,
+            cards.get_remaining_cards_to_choose(),
+            0,
+            put_vol,
+        ),
+    )
+
+    print("Call Volatility:", call_vol)
+
+    print("Put Volatility:", put_vol)
+    print(
+        "Call Delta:",
+        call_delta(
+            cards.get_theoretical_price(),
+            150,
+            cards.get_remaining_cards_to_choose(),
+            0,
+            call_vol,
+        ),
+    )
+    print(
+        "Put Delta:",
+        put_delta(
+            cards.get_theoretical_price(),
+            130,
+            cards.get_remaining_cards_to_choose(),
+            0,
+            put_vol,
+        ),
+    )
+    end_time = time.time()
+    print("Time taken:", end_time - start_time, "seconds")
