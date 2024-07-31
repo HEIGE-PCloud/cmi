e825609873fae332224746a817c4fd9a461297a2
PCloud <heige.pcloud@outlook.com>
2024-07-31 19:54:18 +0200

    refactor(ui): class design

--- ui.py
+++ ui.py
@@ -2,8 +2,11 @@ import datetime
 from bokeh.server.server import Server
 from tornado.ioloop import IOLoop
 from bokeh.document import Document
-from bokeh.models import Div, RadioButtonGroup, Button, ColumnDataSource, DataTable, TableColumn
-from bokeh.layouts import layout, row
+from bokeh.models import ColumnDataSource, RadioButtonGroup, Div, Button
+from bokeh.layouts import layout
+from bokeh.plotting import figure
+import numpy as np
+
 from typing import Callable, List, Optional
 
 from exchange import Exchange
@@ -12,105 +15,92 @@ from trade_config import TradeConfig
 IP_ADDRESS = "127.0.0.1"
 
 
-callback_functions: List[Callable] = []
+class CardsUI:
 
+    def __init__(self, trade_config: TradeConfig):
+        self.trade_config = trade_config
+        self.trade_config.get_cards_value = self.get_cards_value
+        self.cards_radio_button_groups = []
+        self.cards_labels = [
+            "Unknown",
+            "A",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "J",
+            "Q",
+            "K",
+        ]
 
-def periodic_callback():
-    for func in callback_functions:
-        func()
+    def render_cards_radio_button_group(self, index: int):
+        def radio_botton_group_on_change(attr: str, old_active: int, new_active: int):
+            self.trade_config.update_cards()
+            print(self.trade_config.cards.get_theoretical_price())
 
+        radio_button_group = RadioButtonGroup(labels=self.cards_labels, active=0)
+        radio_button_group.on_change("active", radio_botton_group_on_change)
 
-def render_cards_radio_button_group(index: int):
-    cards_labels = [
-        "Unknown",
-        "A",
-        "2",
-        "3",
-        "4",
-        "5",
-        "6",
-        "7",
-        "8",
-        "9",
-        "10",
-        "J",
-        "Q",
-        "K",
-    ]
+        div = Div(text=f"Card {index:02}")
+        return layout([[div, radio_button_group]])
 
-    def radio_botton_group_on_change(attr: str, old_active: int, new_active: int):
-        global trade_config
-        trade_config.update_cards()
-        print(trade_config.cards.get_theoretical_price())
+    def render(self):
+        self.cards_radio_button_groups = [
+            self.render_cards_radio_button_group(i) for i in range(1, 21)
+        ]
 
-    radio_button_group = RadioButtonGroup(labels=cards_labels, active=0)
-    radio_button_group.on_change("active", radio_botton_group_on_change)
+        def reset_all_button_handler():
+            for radio_button_group in self.cards_radio_button_groups:
+                radio_button_group.children[0].children[1].active = 0
 
-    div = Div(text=f"Card {index:02}")
-    return layout([[div, radio_button_group]])
+        reset_all_button = Button(label="Reset all", button_type="danger")
+        reset_all_button.on_event("button_click", reset_all_button_handler)
+        return layout([self.cards_radio_button_groups], reset_all_button)
 
-
-def render_cards_selection():
-    cards_radio_button_groups = [
-        render_cards_radio_button_group(i) for i in range(1, 21)
-    ]
-
-    def reset_all_button_handler():
-        for radio_button_group in cards_radio_button_groups:
-            radio_button_group.children[0].children[1].active = 0
-
-    def get_cards_value():
+    def get_cards_value(self):
         res = []
-        for radio_button_group in cards_radio_button_groups:
+        for radio_button_group in self.cards_radio_button_groups:
             val = radio_button_group.children[0].children[1].active
             if val != 0:
                 res.append(val)
         return res
 
-    reset_all_button = Button(label="Reset all", button_type="danger")
-    reset_all_button.on_event("button_click", reset_all_button_handler)
 
-    # cards_radio_button_groups.append(reset_all_button)
-
-    return layout([cards_radio_button_groups], reset_all_button), get_cards_value
-
-
-def build_simple_datasource():
-    source = ColumnDataSource(data=dict(data=[]))
-
-    def update():
-        source.stream({"data": [datetime.datetime.now()]}, rollover=10)
-
-    callback_functions.append(update)
-    return source
-
-
-def ui_root(doc: Document):
-    global cmi
-    source = build_simple_datasource()
-    div = Div(text=f"Current rank: {cmi.get_rank()}")
-    data_table = DataTable(source=source, columns=[TableColumn(field="data", title="Data")])
-    cards_selection, get_cards_value = render_cards_selection()
-    trade_config.get_cards_value = get_cards_value
-
-    doc.add_root(layout([div, data_table, cards_selection]))
-
-    doc.add_periodic_callback(periodic_callback, 500)
-
-
-class UI():
-    
-    def __init__(self, exchange: Exchange, trade_config: TradeConfig) -> None:
+class MainUI:
+    def __init__(self, exchange: Exchange, config: TradeConfig):
         self.exchange = exchange
-        self.trade_config = trade_config
+        self.config = config
 
-def start_ui(exchange: Exchange, tc: TradeConfig):
-    global cmi
-    cmi = exchange
-    global trade_config
-    trade_config = tc
+    def ui_root(self, doc: Document):
+        update_functions = []
+        c = CardsUI(self.config)
+        source = ColumnDataSource(data=dict(x=[], y=[]))
+        p = figure(title="Random Data Plot", x_axis_label="x", y_axis_label="y")
+        p.line("x", "y", source=source, line_width=2)
+        doc.add_root(layout([p, c.render()]))
+
+        def update():
+            new_data = dict(x=[np.random.random()], y=[np.random.random()])
+            source.stream(new_data, rollover=100)
+
+        update_functions.append(update)
+
+        def periodic_callbacks():
+            for update_function in update_functions:
+                update_function()
+
+        doc.add_periodic_callback(periodic_callbacks, 1000)
+
+
+def start_ui(exchange: Exchange, trade_config: TradeConfig):
+    ui = MainUI(exchange, trade_config)
     server = Server(
-        {"/": ui_root},
+        {"/": ui.ui_root},
         num_procs=1,
         io_loop=IOLoop(),
         address=IP_ADDRESS,
@@ -118,9 +108,8 @@ def start_ui(exchange: Exchange, tc: TradeConfig):
     )
     server.start()
     print(f"Starting turbot ui on http://{IP_ADDRESS}:5006/")
-    # server.io_loop.add_callback(server.show, "/")
     server.io_loop.start()
 
 
-# if __name__ == "__main__":
-#     start_ui()
+if __name__ == "__main__":
+    start_ui(None, TradeConfig())
