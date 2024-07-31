88e173399528777c65e3247a0f08d403cdeb7086
Junrong Chen <junrong.chen@imc.com>
2024-07-31 18:55:43 +0200

    feat: update ui

--- ui.py
+++ ui.py
@@ -2,24 +2,41 @@ import datetime
 from bokeh.server.server import Server
 from tornado.ioloop import IOLoop
 from bokeh.document import Document
-from bokeh.models import Div, RadioButtonGroup, Button
+from bokeh.models import Div, RadioButtonGroup, Button, ColumnDataSource, DataTable, TableColumn
 from bokeh.layouts import layout, row
 from typing import Callable, List, Optional
 
 from exchange import Exchange
 from trade_config import TradeConfig
 
-IP_ADDRESS = "0.0.0.0"
+IP_ADDRESS = "127.0.0.1"
 
 
 callback_functions: List[Callable] = []
+
 
 def periodic_callback():
     for func in callback_functions:
         func()
 
+
 def render_cards_radio_button_group(index: int):
-    cards_labels = ["Unknown", "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    cards_labels = [
+        "Unknown",
+        "A",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+        "J",
+        "Q",
+        "K",
+    ]
 
     def radio_botton_group_on_change(attr: str, old_active: int, new_active: int):
         global trade_config
@@ -27,23 +44,26 @@ def render_cards_radio_button_group(index: int):
         print(trade_config.cards.get_theoretical_price())
 
     radio_button_group = RadioButtonGroup(labels=cards_labels, active=0)
-    radio_button_group.on_change('active', radio_botton_group_on_change)
-    
+    radio_button_group.on_change("active", radio_botton_group_on_change)
+
     div = Div(text=f"Card {index:02}")
     return layout([[div, radio_button_group]])
 
+
 def render_cards_selection():
-    cards_radio_button_groups = [render_cards_radio_button_group(i) for i in range(1, 21)]
-    
+    cards_radio_button_groups = [
+        render_cards_radio_button_group(i) for i in range(1, 21)
+    ]
+
     def reset_all_button_handler():
         for radio_button_group in cards_radio_button_groups:
             radio_button_group.children[0].children[1].active = 0
-    
+
     def get_cards_value():
         res = []
         for radio_button_group in cards_radio_button_groups:
             val = radio_button_group.children[0].children[1].active
-            if val != 0:            
+            if val != 0:
                 res.append(val)
         return res
 
@@ -53,16 +73,30 @@ def render_cards_selection():
     # cards_radio_button_groups.append(reset_all_button)
 
     return layout([cards_radio_button_groups], reset_all_button), get_cards_value
-        
+
+
+def build_simple_datasource():
+    source = ColumnDataSource(data=dict(data=[]))
+
+    def update():
+        source.stream({"data": [datetime.datetime.now()]}, rollover=10)
+
+    callback_functions.append(update)
+    return source
+
 
 def ui_root(doc: Document):
     global cmi
-    div = Div(text=f"Current rank: {cmi.get_rank()} {datetime.datetime.now()}")
+    source = build_simple_datasource()
+    div = Div(text=f"Current rank: {cmi.get_rank()}")
+    data_table = DataTable(source=source, columns=[TableColumn(field="data", title="Data")])
     cards_selection, get_cards_value = render_cards_selection()
     trade_config.get_cards_value = get_cards_value
-    
-    doc.add_root(layout([div, cards_selection]))
-    doc.add_periodic_callback(periodic_callback, 1000)
+
+    doc.add_root(layout([div, data_table, cards_selection]))
+
+    doc.add_periodic_callback(periodic_callback, 500)
+
 
 def start_ui(exchange: Exchange, tc: TradeConfig):
     global cmi
