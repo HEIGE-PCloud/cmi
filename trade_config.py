069d85e5e15e83da40015e333e49aebc64d79248
PCloud <heige.pcloud@outlook.com>
2024-07-31 23:40:07 +0200

    feat: make pricing async

--- trade_config.py
+++ trade_config.py
@@ -1,11 +1,14 @@
 from cards import Cards
 from option_pricing import compile_option_pricing_cpp, option_pricing_cpp
+import threading
+import queue
 
 
 class TradeConfig():
     
     def __init__(self) -> None:
         compile_option_pricing_cpp()
+        self.pricing = Pricing(self)
         self.cards = Cards()
         self.get_cards_value = None
         self.future_theo = None
@@ -18,12 +21,13 @@ class TradeConfig():
         self.update_theo()        
 
     def update_cards(self):
+        self.reset_theo()
         self.cards.set_chosen_cards(self.get_cards_value())
         self.update_theo()
 
     def update_theo(self):
         self.future_theo = self.cards.get_theoretical_price()
-        self.call_theo, self.put_theo, self.call_delta, self.put_delta = option_pricing_cpp(self.cards, self.threads, self.iterations)    
+        self.pricing.pricing()
     
     def reset_theo(self):
         self.call_theo = None
@@ -31,3 +35,21 @@ class TradeConfig():
         self.call_delta = None
         self.put_delta = None
         self.future_theo = None
+
+class Pricing():
+
+    def __init__(self, config: TradeConfig) -> None:
+        self.config = config
+        self.queue = queue.Queue()
+        self.pricing_thread = threading.Thread(target=self.option_pricing_cpp_thread, daemon=True)
+        self.pricing_thread.start()
+
+    def option_pricing_cpp_thread(self):
+
+        while True:
+            x = self.queue.get()
+            self.config.call_theo, self.config.put_theo, self.config.call_delta, self.config.put_delta = option_pricing_cpp(self.config.cards, self.config.threads, self.config.iterations)
+            self.queue.task_done()
+    
+    def pricing(self):
+        self.queue.put(1)
