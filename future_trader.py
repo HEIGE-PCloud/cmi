d8fb76c85ef50c574c23360d8a035362468e7540
Junrong Chen <junrong.chen@imc.com>
2024-08-08 14:16:10 +0200

    chore: update future trader

--- future_trader.py
+++ future_trader.py
@@ -13,24 +13,21 @@ def main():
     cards = Cards()
     while True:
         news = cmi.get_news()
-        if news is not None:
-            size = len(news.root)
-            if size == cards.get_chosen_cards_num():
-                pass
-            else:
-                cards.set_chosen_cards(news_to_cards(news))
-                cmi.delete_all_orders()
-                theo = cards.get_theoretical_price()
-                bid_price = round_down_to_tick(theo, 0.5)
-                ask_price = round_up_to_tick(theo, 0.5)
-                if bid_price == ask_price:
-                    ask_price += 0.5
-                logging.info(f"Making market at {bid_price} - {ask_price}")
-                cmi.insert_order("FUTURE", price=bid_price, volume=200, side=Side.BUY)
-                cmi.insert_order("FUTURE", price=ask_price, volume=200, side=Side.SELL)
+        cards = news_to_cards(news)
+        size = len(cards)
+        if size == cards.get_chosen_cards_num():
+            pass
         else:
-            logging.info("Delete all orders")
+            cards.set_chosen_cards(news_to_cards(news))
             cmi.delete_all_orders()
+            theo = cards.get_theoretical_price()
+            bid_price = round_down_to_tick(theo, 0.5)
+            ask_price = round_up_to_tick(theo, 0.5)
+            if bid_price == ask_price:
+                ask_price += 0.5
+            logging.info(f"Making market at {bid_price} - {ask_price}")
+            cmi.insert_order("FUTURE", price=bid_price, volume=200, side=Side.BUY)
+            cmi.insert_order("FUTURE", price=ask_price, volume=200, side=Side.SELL)
     
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
